e5c37ac083a222687b9870f71baa05d1c0c1cdff
dkaneshi <dkaneshi@gmail.com>
2025-07-07 22:04:29 -1000

    test: add feature tests for post index access validation

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -1,7 +1,29 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\User;
 
-    $response->assertStatus(200);
+test('unauthenticated users are sent to login screen', function () {
+    $response = $this->get(route('posts.index'));
+
+    expect($response)->assertRedirect(route('login'));
+});
+
+test('admin can see posts list', function () {
+    $user = User::factory()->admin()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->get(route('posts.index'));
+
+    expect($response)->assertOk();
+});
+
+test('user cannot see posts list', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->get(route('posts.index'));
+
+    expect($response)->assertStatus(403);
 });
